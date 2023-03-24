92ac7327537ac99e63035bdc68b7778ef28d36ac
Anastasiia Bevziuk <anastasiiabevziuk@gmail.com>
2023-03-24 19:42:07 +0200

    add JSX for Header and Items pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,41 @@
+import { Dropdown, Input, Menu } from 'semantic-ui-react'
+
+
+const languageOptions = [
+    { key: 'English', text: 'English', value: 'English' },
+    { key: 'Українська', text: 'Українська', value: 'Українська' },
+]
 
 function Header() {
     return (
 
-        <p>
-            Header
-        </p>
+        <div>
+            <Menu className='Menu-Header' attached='top'>
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                        <Input icon='search' placeholder='Search...' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Dropdown
+                            button
+                            className='icon Menu-Header'
+                            floating
+                            labeled
+                            icon='world'
+                            options={languageOptions}
+                            search
+                            text='Українська'
+                        />
+                    </Menu.Item>
+                    <Menu.Item
+                        name='Вихід'
+                        active={'logout' === 'logout'}
+                        onClick={() => console.log("log")}
+                        className='Menu-Header'
+                    />
+                </Menu.Menu>
+            </Menu>
+        </div>
 
 
     );
--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,11 +1,51 @@
-
+import { List, Segment, Icon, Input, Checkbox } from 'semantic-ui-react'
 
 function Items() {
     return (
+        <div>
+            <h1>Елементи</h1>
+            <Segment className="Segment-Add" inverted>
+                <Input
+                    action={{
+                        color: 'teal',
+                        labelPosition: 'right',
+                        icon: 'add',
+                        content: 'Add',
+                        inverted: true,
+                    }}
+                    defaultValue=''
+                />
+            </Segment>
+            <Segment inverted color='teal'>
+                <h4>В процесі</h4>
+                <List divided verticalAlign='middle' className='Lists-Container' >
 
-        <p>
-            Items
-        </p>
+                    <List.Item className="Lists-Items" onClick={() => console.log("item")} as='a'>
+                        <List.Content floated='right'>
+                            <Icon name='favorite' />
+                        </List.Content>
+                        <List.Content floated='left'>
+                            <Checkbox radio />
+                        </List.Content>
+                        <List.Content>To Do</List.Content>
+                    </List.Item>
+                </List>
+            </Segment >
+
+            <Segment inverted color='teal'>
+                <h4>Історія</h4>
+
+                <List divided verticalAlign='middle' className='Lists-Container' >
+
+                    <List.Item className="Lists-Items" onClick={() => console.log("item")} as='a'>
+                        <List.Content floated='left'>
+                            <Checkbox radio checked />
+                        </List.Content>
+                        <List.Content>To Do</List.Content>
+                    </List.Item>
+                </List>
+            </Segment >
+        </div >
 
     );
 }
