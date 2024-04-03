969d174aa5ec9ff96dd5e851c17b76c98090a7b5
Anastasiia Bevziuk <anastasiiabevziuk@gmail.com>
2024-04-03 19:42:59 +0300

    add dockerfile, upd env

---
+++ src/auth_config.tsx
@@ -0,0 +1,4 @@
+export const domain = process.env.REACT_APP_DOMAIN as string;
+export const clientId = process.env.REACT_APP_CLIENTID as string;
+
+export const isTest = process.env.REACT_APP_ISTEST === 'true';
